a9db32d5deb6ac674c35b25211b8c579089707df
yasminsantana2024 <00001078235089SP@aluno.educacao.sp.gov.br>
2024-11-21 17:57:38 -0300

    terceiro projeto

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguegem de programação'
+)
+
+criacartao(
+    'Geogria',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criacartao(
+    'Programação',
+    'O que é uma função?',
+    "uma função é um bloco de código que executa uma tarefa"
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?'
+    'Oi em ingles é Hi (RAI)'
+)
